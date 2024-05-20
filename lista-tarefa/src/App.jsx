2afdf4c4ac24d3c6c74a6a1cf9cabadc7330c3ff
GraziellyRaissa1 <grazyraissa7@gmail.com>
2024-05-20 01:00:07 -0300

    first commit

---
+++ lista-tarefa/src/App.jsx
@@ -0,0 +1,25 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react'; 
+
+import Lista from './Components/Lista/Lista'; 
+
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <Lista /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App;
---
+++ lista-tarefa/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Lista from './Components/Lista/Lista.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Lista/>
+  </React.StrictMode>,
+)
